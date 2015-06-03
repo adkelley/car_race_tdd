be505c5db8bc54c77c1ad17ac72e18c558d99a64
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-03 16:58:09 -0700

    final solution

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,31 @@
 require_relative 'car'
-
 class Race
-  #write Race class code here
+  attr_reader :cars, :winner, :loser
+  
+  def initialize
+    @cars = [ Car.new, Car.new ]
+    prng = Random.new
+    @cars[0].accelerate(prng.rand(100))
+    @cars[1].accelerate(prng.rand(100))
+  end
+
+  def winner
+    if (@cars[0].speed > @cars[1].speed)
+      @loser = @cars[1]
+      @cars[0]
+    else
+      @loser = @cars[0]
+      @cars[1]
+    end
+  end
+
+  def loser
+    if (@cars[0].speed < @cars[1].speed)
+      @winner = @cars[1]
+      @cars[0]
+    else
+      @winner = @cars[0]
+      @cars[1]
+    end
+  end
 end
